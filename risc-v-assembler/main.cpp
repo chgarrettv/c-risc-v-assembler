9e5f8c4a31e6706756d2cac09dc7df69ad72289e
chgarrettv <claudehgarrettv@hotmail.com>
2020-09-28 17:50:39 -0500

    Finished RTypes. Working on lw's and sw's.
    + Finished translation for RType instructions.
    + Added a function to find the immediate and source registers for instructions with imm(reg) format.

--- risc-v-assembler/main.cpp
+++ risc-v-assembler/main.cpp
@@ -11,7 +11,7 @@ This program was designed as a basic assembler for RV32I.
 
 using namespace std;
 
-int argToReg(string arg){
+int argToReg(string arg){ // Should return a value between 0-31.
     if(arg == "zero") return 0;
     if(arg == "ra") return 1;
     if(arg == "sp") return 2;
@@ -30,10 +30,34 @@ int argToReg(string arg){
     if(arg == "fp") return 8;
     if(arg[0] == 'a') return stoi(arg.substr(1, arg.length())) + 10;
     if(arg[0] == 'x') return stoi(arg.substr(1, arg.length()));
+
+    else return -1;
 }
 
 int argToImm(string arg) {
     return stoi(arg);
+}
+
+string* addressExpander(string* args) { // For instructions like lw that have a #(reg) in the assembly instruction format. This expands them into reg and immediate arguments.
+    string* ret; ret = new string[args.length() + 1];
+
+    for(int i = 0; i < args.length(); i++) {
+        if(args[i].find("(") != -1 && args.find(")") != -1) {
+            // Initialize the return array.
+            for(int i = 0; i < args.length() - 1; i++) {
+                ret[i] = args[i];
+            }
+
+            string lastArg = args[args.length() - 1];
+            ret[args.length() - 1] = lastArg.substr(lastArg.find("("), lastArg.find(")") - lastArg.find("(")); // rs1
+            ret[args.length()] = lastArg.substr(0, lastArg.find("(")); // imm
+            return ret;
+        }
+        if((args[i].find("(") == -1 && args.find(")") != -1) || (args[i].find("(") != -1 && args.find(")") == -1)) {
+            cout << "Incomplete parenthesis: " << args[i];
+        }
+    }
+    return args;
 }
 
 int RType(string* args, int f7, int f3, int op) {
@@ -184,7 +208,7 @@ int main()
 
     while(getline(prog, inst)) {
         // Cut out any comments.
-        if(inst.find("#")) inst = inst.substr(0, inst.find("#"));
+        if(inst.find("#")) inst = inst.substr(0, inst.find("#")); // If there is a comment, remove it from the instruction.
         if(inst.length() < 2) {lineNumber++; continue;} // If the line is just a comment, skip it.
 
         cout << "Instruction is \"" << inst << "\"\n";
@@ -196,12 +220,18 @@ int main()
         int numArgs = 0;
 
         while(inst.find(",") != -1) { // When a comma is found, there are still multiple arguments left. This finds them.
-            arg[numArgs] = inst.substr(0, inst.find(",")); // The arguments usually end in a comma.
+            int start = 0;
+            while(start < inst.length() && inst[start] == ' ') start++; // Increment start until it is no longer a space. Used to ignore white space.
+
+            arg[numArgs] = inst.substr(start, inst.find(",") - start); // The arguments usually end in a comma.
             numArgs++;
             inst = inst.substr(inst.find(",") + 2, inst.length()); // Remove the argument you just isolated. This only supports a format with ARG, ARG where a space/comma is in between the args.
         }
 
-        arg[numArgs] = inst; // Last piece has to be an argument.
+        // Last piece has to be an argument.
+        if(inst.find(" ") != -1) arg[numArgs] = inst.substr(inst.find_last_of(" ") + 1, inst.length());
+        else arg[numArgs] = inst;
+
         numArgs++;
 
         for(int i = 0; i < numArgs; i++) { // Print out arguments for debugging.
@@ -213,87 +243,91 @@ int main()
             operation[i] = tolower(operation[i]);
         }
 
+
+        // RV 32I Instructions:
                if(operation == "lui") {
-
+            hardwareInst = UType();
         } else if(operation == "auipc") {
-
+            hardwareInst = UType();
         } else if(operation == "jal") {
-
+            hardwareInst = JType();
         } else if(operation == "jalr") {
-
+            hardwareInst = IType();
         } else if(operation == "beq") {
-
+            hardwareInst = BType();
         } else if(operation == "bne") {
-
+            hardwareInst = BType();
         } else if(operation == "blt") {
-
+            hardwareInst = BType();
         } else if(operation == "bge") {
-
+            hardwareInst = BType();
         } else if(operation == "bltu") {
-
+            hardwareInst = BType();
         } else if(operation == "bgeu") {
-
+            hardwareInst = BType();
         } else if(operation == "lb") {
-
+            hardwareInst = IType();
         } else if(operation == "lh") {
-
-        } else if(operation == "lw") {
-
+            hardwareInst = IType();
+        } else if(operation == "lw") { // Think this is for all load and store: format is lw rd, #(rs1) where # is an added offset to rs1. Offset = imm.
+            hardwareInst = IType();
         } else if(operation == "lbu") {
-
+            hardwareInst = IType();
         } else if(operation == "lhu") {
-
+            hardwareInst = IType();
         } else if(operation == "sb") {
-
+            hardwareInst = SType();
         } else if(operation == "sh") {
-
+            hardwareInst = SType();
         } else if(operation == "sw") {
-
+            hardwareInst = SType();
         } else if(operation == "addi") {
-
+            hardwareInst = IType();
         } else if(operation == "slti") {
-
+            hardwareInst = IType();
         } else if(operation == "sltiu") {
-
+            hardwareInst = IType();
         } else if(operation == "xori") {
-
+            hardwareInst = IType();
         } else if(operation == "ori") {
-
+            hardwareInst = IType();
         } else if(operation == "andi") {
-
-        } else if(operation == "slli") {
-
-        } else if(operation == "srli") {
-
-        } else if(operation == "srai") {
-
+            hardwareInst = IType();
+        } else if(operation == "slli") { // NEED A SPECIAL IMM FOR SHAMT
+            hardwareInst = IType();
+        } else if(operation == "srli") { // NEED A SPECIAL IMM FOR SHAMT
+            hardwareInst = IType();
+        } else if(operation == "srai") { // NEED A SPECIAL IMM FOR SHAMT
+            hardwareInst = IType();
         } else if(operation == "add") {
-
+            hardwareInst = RType(arg, 0x0, 0x0, 0x33);
         } else if(operation == "sub") {
-
+            hardwareInst = RType(arg, 0x2, 0x0, 0x33);
         } else if(operation == "sll") {
-
+            hardwareInst = RType(arg, 0x0, 0x1, 0x33);
         } else if(operation == "slt") {
-
+            hardwareInst = RType(arg, 0x0, 0x2, 0x33);
         } else if(operation == "sltu") {
-
+            hardwareInst = RType(arg, 0x0, 0x3, 0x33);
         } else if(operation == "xor") {
-
+            hardwareInst = RType(arg, 0x0, 0x4, 0x33);
         } else if(operation == "srl") {
-
+            hardwareInst = RType(arg, 0x0, 0x5, 0x33);
         } else if(operation == "sra") {
-
+            hardwareInst = RType(arg, 0x2, 0x5, 0x33);
         } else if(operation == "or") {
-
+            hardwareInst = RType(arg, 0x0, 0x6, 0x33);
         } else if(operation == "and") {
-
+            hardwareInst = RType(arg, 0x0, 0x7, 0x33);
         } else if(operation == "fence") {
-
-        } else if(operation == "ecall") {
-
-        } else if(operation == "ebreak") {
-
+            hardwareInst = IType();
+        } else if(operation == "ecall") { // Hard coded instruction.
+            hardwareInst = 0x00000073;
+        } else if(operation == "ebreak") { // Hard coded instruction.
+            hardwareInst = 0x00100073;
         }
+
+        cout << hex << setfill('0') << setw(8) << hardwareInst;
 
         // Prepare for next cycle.
         lineNumber++;
